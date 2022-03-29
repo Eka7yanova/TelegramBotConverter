7e23d5cc73acdb9c195969e47f4af6959ea6e7a9
Eka7yanova <tts.work.24@gmail.com>
2022-03-29 18:26:34 +0300

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,10 @@
+TOKEN='5280059842:AAEdIUNhbRmotYsdiSewXYVTx_ez75haJKw'
+
+keys={
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'eвро': 'EUR',
+    'лайткоин': 'LTC',
+    'рубль': 'RUB'
+}
